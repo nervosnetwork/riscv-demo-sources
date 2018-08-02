51080bba1e6a7ebf21b3b7c8b83b1ce9b049f06f
Xuejie Xiao <xxuejie@gmail.com>
2018-08-02 11:24:26 +0800

    Add nolibc demo

---
+++ nolibc/main.c
@@ -0,0 +1,9 @@
+/*
+ * This is a demostration on how small we can achieve for a C program on RISC-V system.
+ *
+ * So far this results in 896 bytes in binary size, and 330 bytes in gzip format
+ */
+int main()
+{
+  return 0;
+}
